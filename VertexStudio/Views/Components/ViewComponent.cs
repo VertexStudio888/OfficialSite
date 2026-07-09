50f5145222dac9721092674fc58ba96785127fb4
VertexStudio888 <vertex.studio.888@outlook.pt>
2026-07-09 20:37:50 +0100

    Last Updates

---
+++ VertexStudio/Views/Components/ViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeuProjeto.ViewComponents
+{
+    // O nome da classe deve terminar em 'ViewComponent'
+    public class NavigationBarDropDownViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Aqui você pode buscar dados do Umbraco se necessário
+            return View();
+        }
+    }
+}
